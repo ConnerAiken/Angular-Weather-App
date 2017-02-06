4a55b8eff109f6b063cbd33646c1533a358e8439
Conner Aiken <conner@fittedtech.com>
2017-02-06 07:18:10 -0800

    Fixed pathing for node_modules

--- app/app.js
+++ app/app.js
@@ -5,12 +5,12 @@ app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
   .state('home', {
     url: '/home',
     controller: 'homeCtrl',
-    templateUrl: 'templates/home.html'
+    templateUrl: 'app/templates/home.html'
   })
   .state('forecast',{
     url: '/forecast/zip/:zip',
     controller: 'forecastCtrl',
-    templateUrl: 'templates/forecast-week.html',
+    templateUrl: 'app/templates/forecast-week.html',
   })
   /* I normally do controllerAs: 'vm' syntax as well */
   $urlRouterProvider.otherwise('home');
